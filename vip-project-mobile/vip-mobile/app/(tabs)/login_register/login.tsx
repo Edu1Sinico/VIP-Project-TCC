cc30478ed4cae90d1eeec7306a5f4ade3cdfe23d
Edu1Sinico <EduardoSenai888@gmail.com>
2024-10-02 21:05:17 -0300

    Melhorando a página de login - 02/10/2024

--- vip-project-mobile/vip-mobile/app/(tabs)/login_register/login.tsx
+++ vip-project-mobile/vip-mobile/app/(tabs)/login_register/login.tsx
@@ -1,13 +1,14 @@
 import { useState } from 'react';
 import { View, StyleSheet, Text, Image, ImageBackground, TextInput, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-const logo = require("@/assets/images/vip_tranportes_logo_transparent.png");
 const background = require("@/assets/images/background/bus_background.png");
 
 
 export default function loginScreen() {
 
     const [email, setEmail] = useState('');  // Estado para armazenar o valor do campo de texto
+    const [user, setUser] = useState('');
     const [password, setPassword] = useState('');
 
     return (
@@ -16,17 +17,33 @@ export default function loginScreen() {
                 <View style={styles.loginSection}>
 
                     <View style={styles.topSection}>
-                        <Image
-                            source={logo}
-                            style={styles.logo}
-                        />
+                        <View style={styles.userSection}>
+                            <Icon name="user" size={110} color="#166CE2" />
+                        </View>
+
                         <Text style={styles.title}>Login</Text>
                     </View>
 
                     <View style={styles.middleSection}>
+
+                        {/* Campo de Usuário */}
+                        <View style={styles.inputSection}>
+                            <View style={styles.iconInputSection}>
+                                <Icon name="user" size={20} color="#fff" />
+                            </View>
+                            <TextInput
+                                style={styles.input}
+                                placeholder="Usuário"
+                                value={user}
+                                onChangeText={setUser}
+                            />
+                        </View>
+
                         {/* Campo do Email */}
                         <View style={styles.inputSection}>
-                            <Text style={styles.label}>E-mail:</Text>
+                            <View style={styles.iconInputSection}>
+                                <Icon name="at" size={20} color="#fff" />
+                            </View>
                             <TextInput
                                 style={styles.input}
                                 placeholder="E-mail"
@@ -39,7 +56,9 @@ export default function loginScreen() {
                         {/* Campo de Senha */}
                         <View style={styles.inputSection}>
                             {/* Campo da Senha */}
-                            <Text style={styles.label}>Senha:</Text>
+                            <View style={styles.iconInputSection}>
+                                <Icon name="lock" size={20} color="#fff" />
+                            </View>
                             <TextInput
                                 style={styles.input}
                                 placeholder="Senha"
@@ -47,16 +66,20 @@ export default function loginScreen() {
                                 value={password}
                                 onChangeText={setPassword} // Atualiza o estado com o texto digitado
                             />
-
-                            <TouchableOpacity style={styles.touchLink}>
-                                <Text style={styles.link}>Esqueceu a senha? Clique aqui!</Text>
-                            </TouchableOpacity>
                         </View>
+
+                        <TouchableOpacity style={styles.touchLink}>
+                            <Text style={styles.link}>Esqueceu a senha? Clique aqui!</Text>
+                        </TouchableOpacity>
+
                     </View>
 
                     <View style={styles.bottomSection}>
                         <TouchableOpacity style={styles.loginButton}>
                             <Text style={styles.loginTextButton}>Login</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.touchLink}>
+                            <Text style={styles.link}>Não possuí uma conta? Registra-se!</Text>
                         </TouchableOpacity>
                     </View>
                 </View>
@@ -78,61 +101,68 @@ const styles = StyleSheet.create({
     },
 
     loginSection: {
+        flex: 1,
         backgroundColor: 'rgba(255, 255, 255, 0.8)',
-        borderRadius: 15,
         borderStyle: "solid",
-        borderWidth: 3,
-        borderColor: "transparent",
-        width: '80%',
-        height: '90%',
+        width: '100%',
+        height: '100%',
         flexDirection: 'column',
         alignItems: 'center',
+        justifyContent: 'center',
     },
 
     topSection: {
         width: '100%',
-        height: 198,
+        height: 250,
         flexDirection: 'column',
         alignItems: 'center',
         justifyContent: 'space-evenly',
     },
 
-    logo: {
-        width: 175,
-        height: 165,
+    userSection: {
+        width: 150,
+        height: 150,
+        backgroundColor: '#FFF',
+        borderRadius: 100,
+        justifyContent: 'center',
+        alignItems: 'center',
     },
 
     title: {
         fontSize: 20,
-        fontWeight: 'bold',
-        color: '#AB9E59',
+        fontWeight: '400',
+        color: '#166CE2',
     },
 
     middleSection: {
         width: '100%',
-        height: 198,
+        height: 200,
         alignItems: 'center',
-        justifyContent: 'space-evenly'
+        justifyContent: 'space-between'
     },
 
     inputSection: {
-        width: '85%',
-        flexDirection: 'column',
+        width: '100%',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
     },
 
-    label: {
-        fontSize: 15,
-        marginBottom: 3,
-    },
 
     input: {
-        height: 40,
-        borderColor: 'transparent',
+        width: '65%',
+        height: 45,
         backgroundColor: '#fff',
-        borderWidth: 1,
-        borderRadius: 15,
         paddingLeft: 10,
         color: '#C7C7C7',
+    },
+
+    iconInputSection: {
+        width: 45,
+        height: 45,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#166CE2',
     },
 
     touchLink: {
@@ -149,7 +179,7 @@ const styles = StyleSheet.create({
 
     bottomSection: {
         width: '100%',
-        height: 198,
+        height: 200,
         justifyContent: 'center',
         alignItems: 'center',
     },
@@ -161,7 +191,7 @@ const styles = StyleSheet.create({
         marginBottom: 20,
         alignItems: 'center',
         justifyContent: 'center',
-        backgroundColor: '#0F48F2',
+        backgroundColor: '#166CE2',
         shadowColor: '#000',
         shadowOffset: { width: 4, height: 4 },
         shadowOpacity: 0.5,
