d5696f4c78fef65d277466762e55317b20acb0a5
Edu1Sinico <EduardoSenai888@gmail.com>
2024-10-16 14:52:49 -0300

    Separando a estilização dos arquivos principais - 16/10/2024

--- vip-project-mobile/vip-mobile/app/(tabs)/login_register/login.tsx
+++ vip-project-mobile/vip-mobile/app/(tabs)/login_register/login.tsx
@@ -1,10 +1,14 @@
 import { useState } from 'react';
-import { View, StyleSheet, Text, Modal, ImageBackground, TextInput, TouchableOpacity } from 'react-native';
+import { View, Text, Modal, ImageBackground, TextInput, TouchableOpacity } from 'react-native';
 import { useLinkTo } from '@react-navigation/native';
+// Importando a estilização
+import styles  from '@/app/styles/login_register/LoginStyle';
+
 import Icon from 'react-native-vector-icons/FontAwesome';
 // Importando a tela de aviso do campo vazio
 import { ModalAlertValidation } from '@/components/modal/ModalAlertValidation';
 import React from 'react';
+
 
 const background = require("@/assets/images/background/bus_background.png");
 
@@ -119,121 +123,3 @@ export default function LoginScreen() {
         </View >
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-
-    backgroundImage: {
-        flex: 1,
-        height: "100%",
-        justifyContent: "center",
-        alignItems: "center",
-    },
-
-    loginSection: {
-        flex: 1,
-        backgroundColor: 'rgba(255, 255, 255, 0.8)',
-        borderStyle: "solid",
-        width: '100%',
-        height: '100%',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-
-    topSection: {
-        width: '100%',
-        height: 250,
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'space-evenly',
-    },
-
-    userSection: {
-        width: 150,
-        height: 150,
-        backgroundColor: '#FFF',
-        borderRadius: 100,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-
-    title: {
-        fontSize: 20,
-        fontWeight: '400',
-        color: '#166CE2',
-    },
-
-    middleSection: {
-        width: '100%',
-        height: 200,
-        alignItems: 'center',
-        justifyContent: 'space-between'
-    },
-
-    inputSection: {
-        width: '100%',
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-
-
-    input: {
-        width: '65%',
-        height: 45,
-        backgroundColor: '#fff',
-        paddingLeft: 10,
-        color: '#C7C7C7',
-    },
-
-    iconInputSection: {
-        width: 45,
-        height: 45,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#166CE2',
-    },
-
-    touchLink: {
-        width: '100%',
-        marginTop: 5,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-
-    link: {
-        color: '#707070',
-        fontSize: 14,
-    },
-
-    bottomSection: {
-        width: '100%',
-        height: 200,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-
-    loginButton: {
-        padding: 15,
-        height: 70,
-        width: 120,
-        marginBottom: 20,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#166CE2',
-        shadowColor: '#000',
-        shadowOffset: { width: 4, height: 4 },
-        shadowOpacity: 0.5,
-        shadowRadius: 10,
-    },
-
-    loginTextButton: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        color: '#fff',
-    },
-
-});
---
+++ vip-project-mobile/vip-mobile/app/styles/login_register/RegisterStyle.tsx
@@ -0,0 +1,130 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+
+    backgroundImage: {
+        flex: 1,
+        height: "100%",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+
+    registerSection: {
+        flex: 1,
+        backgroundColor: 'rgba(255, 255, 255, 0.8)',
+        borderStyle: "solid",
+        width: '100%',
+        height: '100%',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+    topSection: {
+        width: '100%',
+        height: 200,
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'space-evenly',
+    },
+
+    userSection: {
+        width: 150,
+        height: 150,
+        backgroundColor: '#FFF',
+        borderRadius: 100,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+
+    logo: {
+        width: 175,
+        height: 165,
+    },
+
+    title: {
+        fontSize: 20,
+        fontWeight: '400',
+        color: '#166CE2',
+    },
+
+    middleSection: {
+        width: '100%',
+        height: 300,
+        alignItems: 'center',
+        justifyContent: 'space-evenly'
+    },
+
+    inputSection: {
+        width: '100%',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+    label: {
+        fontSize: 15,
+        marginBottom: 3,
+    },
+
+    input: {
+        width: '65%',
+        height: 45,
+        backgroundColor: '#fff',
+        paddingLeft: 10,
+        color: '#C7C7C7',
+    },
+
+    iconInputSection: {
+        width: 45,
+        height: 45,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#166CE2',
+    },
+
+
+    touchLink: {
+        width: '100%',
+        marginTop: 5,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+    link: {
+        color: '#707070',
+        fontSize: 14,
+    },
+
+    bottomSection: {
+        width: '100%',
+        height: 100,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    registerButton: {
+        padding: 15,
+        height: 70,
+        width: 120,
+        marginBottom: 20,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#166CE2',
+        shadowColor: '#000',
+        shadowOffset: { width: 4, height: 4 },
+        shadowOpacity: 0.5,
+        shadowRadius: 10,
+    },
+
+    registerTextButton: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: '#fff',
+    },
+
+});
