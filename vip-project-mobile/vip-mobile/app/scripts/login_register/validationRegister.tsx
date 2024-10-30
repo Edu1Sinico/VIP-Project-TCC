1eb73939ddd18c82951088c8353d3dd5bd006b92
Edu1Sinico <EduardoSenai888@gmail.com>
2024-10-30 20:02:04 -0300

    Concertando os problemas de validações causadas pelo versionamento - 30/10/2024

--- vip-project-mobile/vip-mobile/app/scripts/login_register/validationRegister.tsx
+++ vip-project-mobile/vip-mobile/app/scripts/login_register/validationRegister.tsx
@@ -1,1 +1,74 @@
-https://github.com/Edu1Sinico/VIP-Project-TCC.git
+export function inputValidationRegister(user, email, password, confirmPassword, setMessageAlert, setUserError, setEmailError, setPasswordError, setConfirmPasswordError, setModalVisible) {
+    let message = "";
+
+    // Verifica se todos os campos estão vazios
+    if (user === '' && email === '' && password === '' && confirmPassword === '') {
+        // Insere a mensagem de validação
+        message = 'Por favor, preencha todos os campos!';
+        setUserError(true);
+        setEmailError(true);
+        setPasswordError(true);
+        setConfirmPasswordError(true);
+        setMessageAlert(message);
+        setModalVisible(true);
+        return false; // Interrompe a execução aqui
+    }
+
+    // Validações para o campo de 'Usuário'
+    if (user === '') {
+        setUserError(true);
+        message = 'O campo de usuário não pode ser vazio!';
+        setMessageAlert(message);
+        setModalVisible(true);
+        return false; // Interrompe a execução aqui
+    } else {
+        setUserError(false);
+    }
+
+    // Validação para Email
+    if (email === '') {
+        message = 'O campo de e-mail não pode ser vazio!';
+        setEmailError(true);
+        setMessageAlert(message);
+        setModalVisible(true);
+        return false; // Interrompe a execução aqui
+    } else if (!email.includes('@') || !email.includes('.com')) {
+        message = 'O campo de e-mail precisa conter o (@) ou (.com)!';
+        setEmailError(true);
+        setMessageAlert(message);
+        setModalVisible(true);
+        return false; // Interrompe a execução aqui
+    } else {
+        setEmailError(false);
+    }
+
+    // Validação do Campo de senha
+    if (password === '') {
+        message = 'O campo de senha não pode ser vazio!';
+        setPasswordError(true);
+        setMessageAlert(message);
+        setModalVisible(true);
+        return false; // Interrompe a execução aqui
+    } else if (password.length < 8) {
+        message = 'O campo de senha precisar ser maior que 8 caracteres!';
+        setPasswordError(true);
+        setMessageAlert(message);
+        setModalVisible(true);
+        return false; // Interrompe a execução aqui
+    } else if (password !== confirmPassword) {
+        message = 'O campo de "senha" e "confirmar senha" precisam ser iguais!';
+        setPasswordError(true);
+        setConfirmPasswordError(true);
+        setMessageAlert(message);
+        setModalVisible(true);
+        return false; // Interrompe a execução aqui
+    } else {
+        setPasswordError(false);
+        setConfirmPasswordError(false);
+    }
+
+    message = 'Usuário cadastrado com sucesso!';
+    setMessageAlert(message);
+    setModalVisible(true);
+    return true; // Se todas as validações passarem
+};
